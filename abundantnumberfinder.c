beaaeebab4194d35b78915e2aa05e957223dec6f
Rhenica <83538157+Rhenica@users.noreply.github.com>
2021-05-02 20:18:51 +0300

    made the program event faster

--- abundantnumberfinder.c
+++ abundantnumberfinder.c
@@ -6,13 +6,13 @@ int maxDivisors = 0;
 long abundantNumber = 0;
 
 int getDivisors(long n) {
-    int divisors = 20;
-    for (long l = 12; l <= n/10; l++) if (n % l == 0) divisors++;
+    int divisors = 19;
+    for (long l = 11; l <= n/10; l++) if (n % l == 0) divisors++;
     return divisors;
 }
 
 int main() {
-    for (long l = 720720; l < 100000000; l += 720720) {
+    for (long l = 1441440; l < 100000000; l += 1441440) {
         const int divisors = getDivisors(l);
         if (divisors > current/3) {
             current++;
