df3529efed1a2dff96b85dbe504b0a24b3e84260
Rhenica <83538157+Rhenica@users.noreply.github.com>
2021-05-02 20:06:22 +0300

    SPEED v2

--- abundantnumberfinder.c
+++ abundantnumberfinder.c
@@ -6,13 +6,13 @@ int maxDivisors = 0;
 long abundantNumber = 0;
 
 int getDivisors(long n) {
-    int divisors = 14;
-    for (long l = 12; l <= n/4; l++) if (n % l == 0) divisors++;
+    int divisors = 20;
+    for (long l = 12; l <= n/10; l++) if (n % l == 0) divisors++;
     return divisors;
 }
 
 int main() {
-    for (long l = 720720; l < 50000000; l += 720720) {
+    for (long l = 50400; l < 10000000; l += 50400) {
         const int divisors = getDivisors(l);
         if (divisors > current/3) {
             current++;
@@ -23,6 +23,6 @@ int main() {
             }
         }
     }
-    printf("%ld has the most divisors out of all numbers between 0 and 50000000 (%d divisors)\n", abundantNumber, maxDivisors);
+    printf("%ld has the most divisors out of all numbers between 0 and 10000000 (%d divisors)\n", abundantNumber, maxDivisors);
     return 0;
 }
