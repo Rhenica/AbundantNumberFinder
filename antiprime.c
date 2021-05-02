1345158e9ec2f138f38b9b2a6a2e11874cb60037
Rhenica <83538157+Rhenica@users.noreply.github.com>
2021-05-02 19:54:28 +0300

    Update antiprime.c

--- antiprime.c
+++ antiprime.c
@@ -3,34 +3,26 @@
 
 int current = 0;
 int maxDivisors = 0;
-int antiPrime = 0;
+long antiPrime = 0;
 
-int getDivisors(int n) {
-    if (n % 35 != 0) return 0;
-    if (n % 70 != 0) return 0;
-    if (n % 96 != 0) return 0;
-    if (n % 192 != 0) return 0;
-    if (n % 160 != 0) return 0;
-    if (n % 320 != 0) return 0;
-    if (n % 480 != 0) return 0;
-    if (n % 960 != 0) return 0;
-    int divisors = 6;
-    for (int i = 6; i <= n/2; i++) if (n % i == 0) divisors++;
+int getDivisors(long n) {
+    int divisors = 12;
+    for (long l = 12; l <= n/2; l++) if (n % l == 0) divisors++;
     return divisors;
 }
 
 int main() {
-    for (int i = 8640; i < 10000000; i += 8640) {
-        const int divisors = getDivisors(i);
+    for (long l = 55440; l < 10000000; l += 55440) {
+        const int divisors = getDivisors(l);
         if (divisors > current/3) {
             current++;
-            printf("anti-prime number found: %d (%d divisors)\n", i, getDivisors(i));
+            printf("anti-prime number found: %ld (%d divisors)\n", l, divisors);
             if (divisors > maxDivisors) {
-                antiPrime = i;
+                antiPrime = l;
                 maxDivisors = divisors;
             }
         }
     }
-    printf("%d has the most divisors out of all numbers between 0 and 10000000 (%d divisors)\n", antiPrime, maxDivisors);
+    printf("%ld has the most divisors out of all numbers between 0 and 10000000 (%d divisors)\n", antiPrime, maxDivisors);
     return 0;
 }
